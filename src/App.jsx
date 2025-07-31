e3c8df53054b2419d59f5634e7062f557c669a5b
jong-seoung <jjong015189@gmail.com>
2025-07-31 11:39:03 +0900

    use-state를 이용한 input, add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Counter from "./components/Counter";
+import InputText from "./components/InputText";
+import ItemAdd from "./components/ItemAdd";
 
 const App = () => {
   return (
-    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+    <div className="bg-red-100 min-h-screen flex justify-center items-center gap-10">
       <Counter />
+      <InputText />
+      <ItemAdd />
     </div>
   );
 };
